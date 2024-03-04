8efecc21e09f50bdcaf33c628603588d4f6ae056
ancalled <ankolt@gmail.com>
2024-03-04 12:56:36 +0200

    Solc 0.8.24

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,7 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.20",
+    version: "0.8.24",
     settings: {
       optimizer: {
         enabled: true,
