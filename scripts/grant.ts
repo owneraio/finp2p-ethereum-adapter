7f4513fa4b3172da265480131873ef7ea8b129e9
ancalled <ankolt@gmail.com>
2024-03-14 11:29:45 +0200

    Add grant roles script

---
+++ scripts/grant.ts
@@ -0,0 +1,23 @@
+import process from "process";
+import { NonceManager, Wallet } from "ethers";
+import { ContractsManager } from "../src/contracts/manager";
+import console from "console";
+
+const grant = async (ethereumRPCUrl: string, finp2pContractAddress: string, deployerPrivateKey: string, operatorAddress: string) => {
+  if (!deployerPrivateKey) {
+    throw new Error("DEPLOYER_PRIVATE_KEY is not set");
+  }
+  console.log("Granting asset manager and transaction manager roles finP2P contract", finp2pContractAddress);
+  const deployer = new NonceManager(new Wallet(deployerPrivateKey));
+  const contractManger = new ContractsManager(ethereumRPCUrl, deployer);
+  await contractManger.grantAssetManagerRole(finp2pContractAddress, operatorAddress);
+  await contractManger.grantTransactionManagerRole(finp2pContractAddress, operatorAddress);
+};
+
+const ethereumRPCUrl = process.argv[2] || "";
+const finp2pContractAddress = process.argv[3] || "";
+const deployerPrivateKey = process.argv[4] || "";
+const operatorAddress = process.argv[5] || "";
+grant(ethereumRPCUrl, finp2pContractAddress, deployerPrivateKey, operatorAddress)
+  .then(() => {
+  });
