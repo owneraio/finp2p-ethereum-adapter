58df3a1c8d1b1ffb7bcad8c5092cc663b0eaa3b7
ancalled <ankolt@gmail.com>
2024-06-03 16:31:24 +0300

    Add extra logging

--- src/operator/service.ts
+++ src/operator/service.ts
@@ -1,5 +1,6 @@
 import { CommonService } from '../services/common';
 import { extractAssetId } from './mapping';
+import { logger } from "../helpers/logger";
 
 export class OperatorService extends CommonService {
 
@@ -7,9 +8,11 @@ export class OperatorService extends CommonService {
     const assetId = extractAssetId(request.asset);
     const issuerFinId = request.to.finId;
     const amount = parseInt(request.balance);
+
+    logger.debug(`Setting balance of ${assetId} to ${issuerFinId}`)
     const txHash = await this.finP2PContract.issue(assetId, issuerFinId, amount);
     await this.finP2PContract.waitForCompletion(txHash);
-
+    logger.debug('Balance minted')
     return {
       isCompleted: true,
     } as Components.Schemas.ReceiptOperation;
