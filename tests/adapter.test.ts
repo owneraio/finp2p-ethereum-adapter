f2ce1d13854aabcff5594713ac30f1592189346f
borod108 <boris.od@gmail.com>
2024-06-17 17:58:18 +0300

    clean

--- tests/adapter.test.ts
+++ tests/adapter.test.ts
@@ -127,7 +127,6 @@ describe(`token service test`, () => {
     );
 
     settlementRef = `${uuidv4()}`;
-    console.log(">>>>>>>>>>>>>>>>>>>>>here>>>>>")
     const redeemReceipt = await client.expectReceipt(await client.tokens.redeem({
       nonce: nonce.toString("hex"),
       source: buyer.account as Components.Schemas.FinIdAccount,
