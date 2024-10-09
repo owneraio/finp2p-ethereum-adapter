9a7a35b177df8b51a35ef2c07434705b55fd0d67
ancalled <ankolt@gmail.com>
2024-10-09 14:08:09 +0300

    Adjust detection

--- finp2p-contracts/src/contracts/model.ts
+++ finp2p-contracts/src/contracts/model.ts
@@ -55,10 +55,8 @@ export const detectError = (e: any) : EthereumTransactionError | NonceToHighErro
     if (e.error.code === -32000 || e.error.message.startsWith('Nonce too high')) {
       return new NonceToHighError(e.error.message);
     }
-  } else if ('code' in e && 'error' in e && 'code' in e.error && 'message' in e.error) {
-    if (e.error.code === -32000 || e.error.message.startsWith('nonce has already been used')) {
+  } else if (`${e}`.includes('nonce has already been used')) {
       return new NonceAlreadyBeenUsedError(e.error.message);
-    }
   }
   return e;
 };
