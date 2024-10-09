e2e0a9319039bb3bb406db0a1c7e3b1cd66ba23c
ancalled <ankolt@gmail.com>
2024-10-09 13:55:09 +0300

    Cover `nonce already been used`

--- finp2p-contracts/src/contracts/model.ts
+++ finp2p-contracts/src/contracts/model.ts
@@ -42,12 +42,22 @@ export class NonceToHighError extends Error {
   }
 }
 
+export class NonceAlreadyBeenUsedError extends Error {
+  constructor(public readonly reason: string) {
+    super(reason);
+  }
+}
+
 export const detectError = (e: any) : EthereumTransactionError | NonceToHighError | Error => {
   if ('code' in e && 'action' in e && 'message' in e && 'reason' in e && 'data' in e) {
     return new EthereumTransactionError(e.reason);
   } else if ('code' in e && 'error' in e && 'code' in e.error && 'message' in e.error) {
     if (e.error.code === -32000 || e.error.message.startsWith('Nonce too high')) {
       return new NonceToHighError(e.error.message);
+    }
+  } else if ('code' in e && 'error' in e && 'code' in e.error && 'message' in e.error) {
+    if (e.error.code === -32000 || e.error.message.startsWith('nonce has already been used')) {
+      return new NonceAlreadyBeenUsedError(e.error.message);
     }
   }
   return e;
