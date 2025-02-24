c7f82af88338d92a8af5796567a0afa0c696c116
ancalled <ankolt@gmail.com>
2025-02-24 11:57:31 +0200

    Add version to contract

--- finp2p-contracts/contracts/token/ERC20/FINP2POperatorERC20.sol
+++ finp2p-contracts/contracts/token/ERC20/FINP2POperatorERC20.sol
@@ -27,6 +27,8 @@ contract FINP2POperatorERC20 is AccessControl, FinP2PSignatureVerifier {
     uint8 public constant LEG_ASSET = 1;
     uint8 public constant LEG_SETTLEMENT = 2;
 
+    string public constant VERSION = "0.22.0";
+
     bytes32 private constant ASSET_MANAGER = keccak256("ASSET_MANAGER");
     bytes32 private constant TRANSACTION_MANAGER = keccak256("TRANSACTION_MANAGER");
 
