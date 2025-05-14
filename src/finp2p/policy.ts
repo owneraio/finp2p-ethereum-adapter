415644b138773911d852d3e7159693303bd9a55f
ancalled <ankolt@gmail.com>
2025-05-14 17:32:57 +0300

    OSS failure fix

--- src/finp2p/policy.ts
+++ src/finp2p/policy.ts
@@ -15,31 +15,33 @@ export class PolicyGetter {
   async getPolicy(assetCode: string, assetType: AssetType): Promise<ProofPolicy> {
     let proof: Proof;
     let domain: ProofDomain | null = null;
-    let configRaw: string
+    let configRaw: string;
     switch (assetType) {
       case AssetType.FinP2P: {
-        try {
-          ({ policies: { proof }, config: configRaw } = await this.ossClient.getAsset(assetCode));
-          domain = parseProofDomain(configRaw);
-          break
-        } catch (e) {
-          console.log(e)
-        }
+        ({ policies: { proof }, config: configRaw } = await this.ossClient.getAsset(assetCode));
+        domain = parseProofDomain(configRaw);
+        break;
       }
-      case AssetType.Cryptocurrency: case AssetType.Fiat: {
-        const orgId = process.env.ORGANIZATION_ID || '';
-        ({ policies: { proof } } = await this.ossClient.getPaymentAsset(orgId, assetCode));
-        break
+      case AssetType.Cryptocurrency:
+      case AssetType.Fiat: {
+        const orgId = process.env.ORGANIZATION_ID || "";
+        const paymentAsset = await this.ossClient.getPaymentAsset(orgId, assetCode);
+        if (paymentAsset) {
+          ({ policies: { proof } } = paymentAsset);
+        } else {
+          return { type: "NoProofPolicy" };
+        }
+        break;
       }
       default:
         throw new Error(`Unknown asset type: ${assetType}`);
     }
 
     switch (proof.type) {
-      case 'NoProofPolicy':
-        return { type: 'NoProofPolicy' }
-      case 'SignatureProofPolicy': {
-        return { ...proof, domain }
+      case "NoProofPolicy":
+        return { type: "NoProofPolicy" };
+      case "SignatureProofPolicy": {
+        return { ...proof, domain };
       }
     }
   }
