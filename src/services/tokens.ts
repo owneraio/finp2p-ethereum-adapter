b34208cbb0d1cf198cb6f94683ca23658f6cd1ed
ancalled <ankolt@gmail.com>
2025-05-15 19:18:22 +0300

    Extend transfer try-catch block

--- src/services/tokens.ts
+++ src/services/tokens.ts
@@ -123,20 +123,13 @@ export class TokenService extends CommonService {
 
   public async transfer(request: Paths.TransferAsset.RequestBody): Promise<Paths.TransferAsset.Responses.$200> {
     const { executionContext } = request;
-    const requestParams: RequestParams = { ...request, type: "transfer" };
-    const eip712Params = extractEIP712Params(requestParams);
     try {
+      const requestParams: RequestParams = { ...request, type: "transfer" };
+      const eip712Params = extractEIP712Params(requestParams);
       this.validateRequest(requestParams, eip712Params);
-    } catch (e) {
-      if (e instanceof RequestValidationError) {
-        logger.error(`Validation error: ${e.reason}`);
-        return failedTransaction(1, e.reason);
-      }
-    }
-    const { buyerFinId, sellerFinId, asset, settlement, loan, params } = eip712Params;
-    const { nonce, signature: { signature } } = request;
+      const { buyerFinId, sellerFinId, asset, settlement, loan, params } = eip712Params;
+      const { nonce, signature: { signature } } = request;
 
-    try {
       const txHash = await this.finP2PContract.transfer(nonce, sellerFinId, buyerFinId, asset, settlement, loan, params, signature);
       if (executionContext) {
         this.execDetailsStore?.addExecutionContext(txHash, executionContext.executionPlanId, executionContext.instructionSequenceNumber);
