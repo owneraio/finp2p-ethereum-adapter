9d3bc45c0a599ac9f22241079321df3edded8735
Andrey Koltochnik <ankolt@gmail.com>
2025-06-25 09:30:31 +0300

    [Task] Support an optional decimal truncator (#61)
    * [Task] Support an optional decimal truncator
    
    * Default test-env param

--- src/services/tokens.ts
+++ src/services/tokens.ts
@@ -25,8 +25,8 @@ export class TokenService extends CommonService {
   assetCreationPolicy: AssetCreationPolicy;
 
   constructor(finP2PContract: FinP2PContract, assetCreationPolicy: AssetCreationPolicy, policyGetter: PolicyGetter | undefined,
-              execDetailsStore: ExecDetailsStore | undefined) {
-    super(finP2PContract, policyGetter, execDetailsStore);
+              execDetailsStore: ExecDetailsStore | undefined, defaultDecimals: number = 18) {
+    super(finP2PContract, policyGetter, execDetailsStore, defaultDecimals);
     this.assetCreationPolicy = assetCreationPolicy;
   }
 
