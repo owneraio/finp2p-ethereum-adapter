97d7fcfb07270dabd08218c4d3398ed49faa35ce
ancalled <ankolt@gmail.com>
2025-09-14 19:08:44 +0300

    Adjust tests

--- src/api-models/index.ts
+++
@@ -1,2 +0,0 @@
-
-// TODO: this is a hack to test tools to access generated models, pick other generator that doesn't depend on namespaces
--- tests/api/mapper.ts
+++ tests/api/mapper.ts
@@ -1,6 +1,9 @@
 import { EIP712Message, EIP712Types, hash, signWithPrivateKey } from "../../finp2p-contracts/src";
+import { LedgerAPI } from "@owneraio/finp2p-nodejs-skeleton-adapter";
 
-export const eip712Signature = async (chainId: number, verifyingContract: string, primaryType: string, types: EIP712Types, message: EIP712Message, signerPrivateKey: string) => {
+export const eip712Signature = async (chainId: number,
+                                      verifyingContract: string, primaryType: string,
+                                      types: EIP712Types, message: EIP712Message, signerPrivateKey: string): Promise<LedgerAPI["schemas"]["signature"]> => {
   const hashVal = hash(chainId, verifyingContract, types, message);
   const signature = await signWithPrivateKey(chainId, verifyingContract, types, message, signerPrivateKey);
   return {
@@ -9,17 +12,17 @@ export const eip712Signature = async (chainId: number, verifyingContract: string
         name: "FinP2P", version: "1", chainId, verifyingContract
       }, primaryType, types: eip712TypesToAPI(types), message: eip712MessageToAPI(message), hash: hashVal
     }, hashFunc: "keccak_256"
-  } as Components.Schemas.Signature;
+  } ;
 };
 
 
-export const eip712TypesToAPI = (types: EIP712Types): Components.Schemas.EIP712Types => {
+export const eip712TypesToAPI = (types: EIP712Types): LedgerAPI["schemas"]["EIP712Types"]=> {
   return {
     definitions: Object.entries(types)
       .map(([name, fields]) => {
         return { name, fields };
       })
-  } as Components.Schemas.EIP712Types;
+  };
 };
 
 
@@ -28,14 +31,14 @@ const isPrimitive = (value: any): boolean => {
 };
 
 export const eip712MessageToAPI = (message: EIP712Message): {
-  [name: string]: Components.Schemas.EIP712TypedValue;
+  [name: string]:  LedgerAPI["schemas"]["EIP712TypedValue"];
 } => {
-  const result: Record<string, Components.Schemas.EIP712TypedValue> = {};
+  const result: Record<string,  LedgerAPI["schemas"]["EIP712TypedValue"]> = {};
   Object.entries(message).forEach(([name, value]) => {
     if (isPrimitive(value)) {
       result[name] = value;
     } else if (typeof value === "object" && value !== null) {
-      result[name] = { ...value } as Components.Schemas.EIP712TypeObject;
+      result[name] = { ...value } as  LedgerAPI["schemas"]["EIP712TypeObject"];
     }
   });
   return result;
