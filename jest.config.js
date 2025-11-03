0564485699a13b1ce7c34c11c561926d43917182
ancalled <ankolt@gmail.com>
2025-11-03 13:52:26 +0200

    Cleanup

--- jest.config.js
+++ jest.config.js
@@ -21,6 +21,9 @@ module.exports = {
     "<rootDir>/src",
     "<rootDir>/tests"
   ],
+  "testMatch": [
+    "<rootDir>/tests/**/*.test.+(ts|tsx|js)"
+  ],
   "transform": {
     "^.+\\.(ts|tsx)$": "ts-jest"
   }
