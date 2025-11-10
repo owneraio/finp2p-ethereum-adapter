a119503012095436737b932251f3e098a5766d9f
Andrey Koltochnik <ankolt@gmail.com>
2025-11-10 19:10:13 +0200

    [Task] Finp2p contract scripts (#114)
    * [Task] Access finp2p-contract scripts
    
    * [Task] NPX running finp2p-contract scripts
    
    * Refactor env parameters passing
    
    * Config parsed from both arf and env
    
    * Apply config parser to all scripts
    
    * Npx config
    
    * Push publish
    
    * Bump version
    
    * Bump version
    
    * Bump version
    
    * Bump version
    
    * Change CI order for publishing
    
    * Update package-lock.json
    
    * Bump version
    
    (cherry picked from commit 367319aaf95cda73aa3f7406df95f4b0b15cedfa)

--- finp2p-contracts/scripts/eip712-domain-params.ts
+++ finp2p-contracts/scripts/eip712-domain-params.ts
@@ -1,26 +1,41 @@
 import { Logger, ConsoleLogger } from "@owneraio/finp2p-adapter-models";
 import { FinP2PContract } from "../src";
-import { createProviderAndSigner, ProviderType } from "./config";
+import { createJsonProvider, parseConfig } from "./config";
 
-const logger: Logger = new ConsoleLogger('info')
+const logger: Logger = new ConsoleLogger("info");
 
 
-const domainParams = async (providerType: ProviderType, finp2pContractAddress: string) => {
-  const { provider, signer } = await createProviderAndSigner(providerType, logger);
+const domainParams = async (
+  operatorPrivateKey: string,
+  ethereumRPCUrl: string,
+  finp2pContractAddress: string
+) => {
+  const { provider, signer } = await createJsonProvider(operatorPrivateKey, ethereumRPCUrl);
   const finp2pContract = new FinP2PContract(provider, signer, finp2pContractAddress, logger);
   const { name, version, chainId, verifyingContract } = await finp2pContract.eip712Domain();
   logger.info(`EIP712 domain: name=${name} version=${version} chainId=${chainId} verifyingContract=${verifyingContract}`);
 };
 
-const providerType = (process.env.PROVIDER_TYPE || "local") as ProviderType;
-const finp2pContractAddress = process.env.FINP2P_CONTRACT_ADDRESS;
-if (!finp2pContractAddress) {
-  throw new Error("FINP2P_CONTRACT_ADDRESS is not set");
-}
+const config = parseConfig([
+  {
+    name: "operator_pk",
+    envVar: "OPERATOR_PRIVATE_KEY",
+    required: true,
+    description: "Operator private key"
+  },
+  {
+    name: "rpc_url",
+    envVar: "RPC_URL",
+    required: true,
+    description: "Ethereum RPC URL"
+  },
+  {
+    name: "finp2p_contract_address",
+    envVar: "FINP2P_CONTRACT_ADDRESS",
+    description: "FinP2P contract address",
+    required: true
+  }
+]);
 
-
-domainParams(providerType, finp2pContractAddress)
-  .catch((err) => {
-    logger.error("Error running domainParams:", err);
-    process.exit(1);
-  });
+domainParams(config.operator_pk!, config.rpc_url!, config.finp2p_contract_address!)
+  .catch(console.error);
