bf6bf9cfad0633a02149437f377688ccf5610268
Shakhzod Ikromov <shakhzod.ikromov@ownera.io>
2026-02-03 17:22:38 +0200

    Tests work for transfer

--- fireblocks_test.sh
+++ fireblocks_test.sh
@@ -37,9 +37,13 @@ curl --fail --retry 30 --retry-all-errors http://localhost:${PORT}/health/readin
 ENDPOINT="http://localhost:${PORT}/api"
 
 TOKEN_NAME=`openssl rand -hex 3`
+TOKEN_NAME='11a260'
 ASSET_RESOURCE_ID=`openssl rand -hex 10`
+ASSET_RESOURCE_ID='81a6a38558ed0c5b910f'
 ISSUE_DESTINATION_FINID="023909c4944fbfdf8c4bc3331d02c0773f04b6483e5da1d61e8e217b5b249c7951"
 TRANSFER_DESTINATION_FINID="03928a764dc1b2c3d3eb48683a4046f4f8cfc6f95a22b6ef3ff9ae400d92c02dbf"
+IDEMPOTENCY_HEADER="Idempotency-Key: `openssl rand -base64 12`"
+IDEMPOTENCY_HEADER='Idempotency-Key: fqb6sx6brU4nnfuh'
 
 REQUEST_BODY=`jq -n --arg ASSET_RESOURCE_ID "$ASSET_RESOURCE_ID" --arg TOKEN_NAME "$TOKEN_NAME" '{
   "asset": {
@@ -57,7 +61,6 @@ REQUEST_BODY=`jq -n --arg ASSET_RESOURCE_ID "$ASSET_RESOURCE_ID" --arg TOKEN_NAM
   "name": "OWNERA"
 }'`
 
-IDEMPOTENCY_HEADER="Idempotency-Key: `openssl rand -base64 12`"
 
 until curl --fail --request POST \
      --url "${ENDPOINT}/assets/create" \
@@ -91,6 +94,46 @@ REQUEST_BODY=`jq -n --arg ASSET_RESOURCE_ID "$ASSET_RESOURCE_ID" --arg ISSUE_DES
 
 until curl --fail --request POST \
      --url "${ENDPOINT}/assets/issue" \
+     --header "$IDEMPOTENCY_HEADER" \
+     --header 'accept: application/json' \
+     --header 'content-type: application/json' \
+     --data "$REQUEST_BODY" | jq -e '.isCompleted' > /dev/null; do
+  sleep 1
+done
+
+REQUEST_BODY=`jq -n --arg ASSET_RESOURCE_ID "$ASSET_RESOURCE_ID" --arg ISSUE_DESTINATION_FINID "$ISSUE_DESTINATION_FINID" --arg TRANSFER_DESTINATION_FINID "$TRANSFER_DESTINATION_FINID" '{
+  "source": {
+    "account": {
+      "type": "finId",
+      "finId": $ISSUE_DESTINATION_FINID
+    },
+    "finId": $ISSUE_DESTINATION_FINID
+  },
+  "destination": {
+    "account": {
+      "type": "finId",
+      "finId": $TRANSFER_DESTINATION_FINID
+    },
+    "finId": $TRANSFER_DESTINATION_FINID
+  },
+  "asset": {
+    "type": "finp2p",
+    "resourceId": $ASSET_RESOURCE_ID
+  },
+  "signature": {
+    "template": {
+      "type": "hashList"
+    },
+    "hashFunc": "unspecified",
+    "signature": "signature"
+  },
+  "nonce": "nonce",
+  "quantity": "0.5",
+  "settlementRef": "settlement"
+}'`
+
+until curl --fail --request POST \
+     --url "${ENDPOINT}/assets/transfer" \
      --header "$IDEMPOTENCY_HEADER" \
      --header 'accept: application/json' \
      --header 'content-type: application/json' \
