553c794e4bf9a8f9048f0a543ac10040d3909f70
Shakhzod Ikromov <shakhzod.ikromov@ownera.io>
2026-03-26 10:31:50 +0200

    cached builds for faster iteration

--- build-docker.sh
+++ build-docker.sh
@@ -4,6 +4,6 @@ set -eu
 
 docker build -t finp2p-ethereum-adapter:latest \
   --secret id=npm_token,env=GITHUB_TOKEN \
-  --no-cache  --progress=plain -f Dockerfile .
+  -f Dockerfile .
 docker tag finp2p-ethereum-adapter:latest localhost:5000/finp2p-ethereum-adapter:latest
 docker push localhost:5000/finp2p-ethereum-adapter:latest
